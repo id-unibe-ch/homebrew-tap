89b5e74ce1804fd83f498847e1a1024920bb423b
Michael Rolli <michael@rollis.ch>
2026-05-06 09:07:45 +0200

    feat: bildschirmuniversum 0.2.1

--- Formula/bildschirmuniversum.rb
+++ Formula/bildschirmuniversum.rb
@@ -1,12 +1,12 @@
 class Bildschirmuniversum < Formula
   desc "A utility to easily manage display arrangements in BüroUniversum."
   homepage "https://github.com/grvlbit/bildschirmUniversum"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   if OS.mac?
-    url "https://github.com/grvlbit/bildschirmUniversum/releases/download/v0.2.0/bildschirmuniversum-macos.zip"
-    sha256 "c6fe6b70e176c8a15e38647f578c1606cf06e8411113ab242dd1e50c2a9b36b2"
+    url "https://github.com/grvlbit/bildschirmUniversum/releases/download/v0.2.1/bildschirmuniversum-macos.zip"
+    sha256 "b6c2241562bd2638e5095654a84ffa0d8d92fb1efaf9ed1f31ee0847f985758f"
   end
 
   def install
@@ -14,6 +14,6 @@ class Bildschirmuniversum < Formula
   end
 
   test do
-    system "#{bin}/bildschirmuniversum --help"
+    system "#{bin}/bildschirmuniversum --version"
   end
 end
